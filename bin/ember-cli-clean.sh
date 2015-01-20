88867961f5e64ad77212d9779b04f778ecd2952a
Matthias Leitner <matthias.leitner@runtastic.com>
2015-01-20 14:39:50 +0100

    extend script

--- bin/ember-cli-clean.sh
+++ bin/ember-cli-clean.sh
@@ -1,1 +1,26 @@
-rm -rf node_modules bower_components tmp dist && npm i && bower i
+delDirs=("dist" "tmp" "node_modules" "bower_components")
+backupDirs=("node_modules" "bower_components")
+backupSuffix="-backup"
+
+echo "start cleaning project reseting and generated files"
+
+for dir in "${backupDirs[@]}"
+do
+  :
+  if [ -d $dir ]
+  then
+    echo "Directory $dir exists. Backing up..."
+    cp -R -f $dir $dir$backupSuffix
+  else
+    echo "Warning: Directory $dir does not exists."
+   fi
+done
+
+
+for dir in "${delDirs[@]}"
+do
+  :
+   echo "deleting $dir"
+   rm -rf $dir
+done
+
